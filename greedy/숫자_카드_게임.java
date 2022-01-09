2e142048f1b78366830fc715235e5a398791a425
ssyoni <jsy950601@gmail.com>
2022-01-09 19:01:02 +0900

    숫자_카드_게임

---
+++ greedy/숫자_카드_게임.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class 숫자_카드_게임 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int min_value = 10001;
+            for (int j = 0; j < m; j++) {
+                int x = scanner.nextInt();
+                min_value = Math.min(min_value, x);
+            }
+            result = Math.max(result, min_value);
+        }
+        System.out.println(result);
+    }
+}
